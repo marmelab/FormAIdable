4723f0baff919c595555755cfed6ab21b3a0b456
Anthony Rimet <anthony.rimet03@gmail.com>
2025-03-11 14:57:14 +0100

    feat: add option page

--- entrypoints/popup/Popup.tsx
+++ entrypoints/popup/Popup.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TabsSelect } from './components/TabsSelect';
 import { Form, FormsList } from './components/FormsList';
 import { useFillForm } from './hooks/useFillForm';
+import { storage } from 'wxt/storage';
 
-function App() {
+const Popup = () => {
     const [selectedTab, setSelectedTab] = useState<number | undefined>(
         undefined,
     );
-
     const [selectedForm, setSelectedForm] = useState<Form | undefined>(
         undefined,
     );
+    const { fillForm, loading, error } = useFillForm();
+    const [openAiKey, setOpenAiKey] = useState<string | null>(null);
 
-    const { fillForm, loading } = useFillForm();
+    useEffect(() => {
+        async function checkOpenAiKey() {
+            const data = await storage.getItem('local:preference');
+            const preference = data as { OPENAI_KEY?: string };
+            setOpenAiKey(preference.OPENAI_KEY || null);
+        }
+        checkOpenAiKey();
+    }, []);
 
     function handleCancel() {
         window.close();
@@ -25,13 +34,49 @@ function App() {
         }
     }
 
+    function handleOpenOption() {
+        browser.runtime.openOptionsPage();
+    }
+
+    if (openAiKey === null) {
+        return (
+            <div className="mx-auto min-w-sm max-w-7xl p-6">
+                <form onSubmit={handleSubmit}>
+                    <div className="space-y-6">
+                        <div className="border-b border-gray-900/10 pb-6">
+                            <h2 className="text-base/7 font-semibold text-gray-900">
+                                FormAIdable
+                            </h2>
+                            <p className="mt-1 text-sm/6 text-gray-600">
+                                Fill your form directly from a tab. Awesome!
+                            </p>
+                        </div>
+                        <p className="text-red-500">
+                            OpenAI key is not initialized. Please set it in the
+                            options.
+                        </p>
+                    </div>
+                </form>
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                    <button
+                        type="button"
+                        className="text-xs/8 italic text-gray-900 cursor-pointer"
+                        onClick={handleOpenOption}
+                    >
+                        Extension option
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="mx-auto min-w-sm max-w-7xl p-6">
             <form onSubmit={handleSubmit}>
                 <div className="space-y-6">
                     <div className="border-b border-gray-900/10 pb-6">
                         <h2 className="text-base/7 font-semibold text-gray-900">
-                            Tab2Form
+                            FormAIdable
                         </h2>
                         <p className="mt-1 text-sm/6 text-gray-600">
                             Fill your form directly from a tab. Awesome!
@@ -58,7 +103,14 @@ function App() {
                         />
                     </div>
                 </div>
-
+                {error && (
+                    <div className="error-message text-red-500">
+                        Error:{' '}
+                        {error instanceof Error
+                            ? error.message
+                            : 'An error occurred while filling the form.'}
+                    </div>
+                )}
                 <div className="mt-6 flex items-center justify-end gap-x-6">
                     <button
                         type="button"
@@ -77,9 +129,19 @@ function App() {
                         {loading ? 'Filling...' : 'Fill form'}
                     </button>
                 </div>
+
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                    <button
+                        type="button"
+                        className="text-xs/8 italic text-gray-900 cursor-pointer"
+                        onClick={handleOpenOption}
+                    >
+                        Extension option
+                    </button>
+                </div>
             </form>
         </div>
     );
-}
+};
 
-export default App;
+export default Popup;
--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     extensionApi: 'chrome',
     modules: ['@wxt-dev/module-react'],
     manifest: {
-        permissions: ['tabs', 'scripting'],
+        permissions: ['tabs', 'scripting', 'storage'],
         host_permissions: ['<all_urls>'],
     },
 });
